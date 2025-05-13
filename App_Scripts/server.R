2dfddacd7ef39990436552d59bfe2ffef7e749ef
Riki Engling <rengling@umd.edu>
2025-05-13 02:07:42 -0400

    Meant to commit after setting up the layout, but was having too much fun, I suppose.
    Proceeded to then spend six hours attempting to figure out the proportions thing for News and finally got one country's done. Now will have to change the graphs and such in the Shiny app first thing.

--- App_Scripts/server.R
+++ App_Scripts/server.R
@@ -3,78 +3,156 @@ library(shinydashboard)
 library(tidyverse) 
 library(vroom) 
 library(plotly)
+library(DT)
 
 
 shinyServer(function(input, output, session){
   
-  #First row + first infobox
-  output$min_ <- renderInfoBox({
-    infoBox(title = "Minm", 
-            value = min(dat.clean$N_TV), 
-            subtitle = "Minimum value for TV in the dataset", 
-            fill = TRUE)
-  })
-  
-  #First row + second infobox
-  output$min_ <- renderInfoBox({
-    infoBox(title = "Maxm", 
-            value = max(dat.clean$N_TV), 
-            subtitle = "Maximum value for TV in the dataset", 
-            fill = TRUE, 
-            color = "yellow")
-  })
-  
-  #First row + third infobox
-  output$min_ <- renderInfoBox({
-    infoBox(title = "SD", 
-            value = sd(dat.clean$N_TV), 
-            subtitle = "Standard deviation value for TV in the dataset", 
-            fill = TRUE, 
-            icon("arrow-up"))
-  })
-  
-  #First row + fourth infobox
-  output$min_ <- renderInfoBox({
-    infoBox(title = "Mean", 
-            value = mean(dat.clean$N_TV), 
-            subtitle = "Mean value for TV in the dataset", 
-            fill = TRUE, 
-            color = "yellow")
-  })
-  
-  
-  
-  
-  
-  
   #Datatable
-  output$mydatatable <- renderDataTable({
-    head(dat.clean)
+  output$mydatatable <- DT::renderDT({
+    head(Dat)
   })
+
+
+#Graphs
   
-  #Plotly Scatter Plot 
-  output$plot1 <- renderPlotly({
-    plot_ly(data = dat.clean, 
-            x = ~Country, 
-            y = ~N_TV, 
-            type = "scatter", 
-            mode = "markers")
+  
+  #Dem by Country 
+  output$dem <- renderPlotly({
+    Dem %>%
+      dplyr::filter(Country == input$selected) %>%
+      dplyr::select(-Country) %>%
+      summarise_all(~ round(mean(.x, na.rm = TRUE), 2)) %>%
+      pivot_longer(cols = everything(), names_to = "Variable", values_to = "Average_Rating") %>% 
+      mutate(Variable = stringr::str_remove(Variable, "^[DNS]_")) %>%
+      plot_ly(
+        x = ~Variable, 
+        y = ~Average_Rating, 
+        type = "bar"
+      )
   })
+    
+
   
-  #Histogram 
-  output$plot2 <- renderPlotly({
+  #News by Country 
+  output$new <- renderPlotly({
+    plot_data <- News %>%
+      dplyr::filter(Country == input$selected) %>%
+      dplyr::select(-Country) %>%
+      pivot_longer(
+        cols = everything(), 
+        names_to = "Variable", 
+        values_to = "Response") %>%
+      group_by(Variable, Response) %>%
+      summarise(Proportion = n() / nrow(News)) %>%
+      mutate(
+        Variable = stringr::str_remove(Variable, "^[N]_"))
     plot_ly(
-      data = dat.clean, 
-      x = ~Country, 
-      y = ~N_TV, 
-      type = "histogram")
+      data = plot_data,
+      x = ~Variable,                
+      y = ~Proportion,              
+      color = ~factor(Response),    
+      type = "bar", 
+      name = ~factor(Response)      
+    ) %>%
+      layout(
+        barmode = "stack",           
+        xaxis = list(title = "Variables"),
+        yaxis = list(title = "Proportion")
+      )
   })
+  
+  #Science 
+  output$sci <- renderPlotly({
+    Sci %>% 
+      dplyr::filter(Country == input$selected) %>%
+      dplyr::select(-Country) %>%
+      summarise_all(~ round(mean(.x, na.rm = TRUE), 2)) %>%
+      pivot_longer(cols = everything(), names_to = "Variable", values_to = "Average_Rating") %>% 
+      mutate(Variable = stringr::str_remove(Variable, "^[DNS]_")) %>%
+      plot_ly(
+        x = ~Variable, 
+        y = ~Average_Rating, 
+        type = "bar") 
+      
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#Tables 
+
+    output$demo <- DT::renderDT({
+    Dem %>% 
+      dplyr::filter(Country == input$selected) %>% 
+      dplyr::select(-Country) %>% 
+      summarise_all(mean, na.rm = TRUE) %>% 
+      round(2) %>% 
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = T))
+  })
+  
+  output$news <- DT::renderDT({
+    News %>% 
+      dplyr::filter(Country == input$selected) %>% 
+      dplyr::select(-Country) %>% 
+      summarise_all(mean, na.rm = TRUE) %>% 
+      round(2) %>% 
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = T))
+  })
+  
+  output$science <- DT::renderDT({
+    Sci %>% 
+      dplyr::filter(Country == input$selected) %>% 
+      dplyr::select(-Country) %>% 
+      summarise_all(mean, na.rm = TRUE) %>% 
+      round(2) %>% 
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = T))
+  })
+
+  
+  #Full data
+  output$bigdem <- DT::renderDT({
+    Dem %>% 
+      group_by(Country) %>%
+      summarise_all(~ round(mean(.x, na.rm = TRUE), 2)) %>% 
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = T))
+  })
+  
+  
+  output$bignew <- DT::renderDT({
+    PropNews%>%
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = TRUE)) 
+  })
+  
+  
+  output$bigsci <- DT::renderDT({
+    Sci %>% 
+      group_by(Country) %>%
+      summarise_all(~ round(mean(.x, na.rm = TRUE), 2)) %>% 
+      rename_with(~ stringr::str_remove(., "^[DNS]_")) %>%
+      DT::datatable(options = list(scrollX = T))
+  })
+  
+  
   
   
   #Input widget 
   output$inputwidget <- renderUI({
-    selectInput(inputId = "in", "select a variable", choices = names(dat.clean))
+    selectInput(inputId = "selected", label = "Select a Country", choices = unique(Dat$Country))
   })
+
+
 }
 )
 
--- App_Scripts/ui.R
+++ App_Scripts/ui.R
@@ -3,69 +3,92 @@ library(shinydashboard)
 library(tidyverse) 
 library(vroom) 
 library(plotly)
+library(DT)
 
 
 
 shinyUI(
   dashboardPage(skin = "blue",
-    dashboardHeader(title = " ", titleWidth = 500, disable = FALSE), 
+    dashboardHeader(title = "World Value Study: Wave '06 Data Exploration", titleWidth = 500, disable = FALSE), 
     
     
-    dashboardSidebar(" ", collapsed = FALSE, disable = FALSE,
+    dashboardSidebar(collapsed = FALSE, disable = FALSE,
                      sidebarMenu( id = 'sidebarmenu', 
-                       menuItem("tabBox", tabName = "charts", icon = icon("check")),
-                       menuItem("box", tabName = "box", icon = icon("check")),
-                                  #first menu item
-                       menuItem(text = "About", tabName = "about", icon = icon("clipboard")), 
+                                  
                        
-                       #Second menu item with 2 submenus
-                       menuItem('chart',
-                         icon = icon('line-chart'), 
-                         menuSubItem('chartmenusub1', 
-                                     tabName = 'chart1', 
-                                     icon = icon('line-chart')), 
-                         menuSubItem('chartsubmenu2', 
-                                     tabName = 'chart2',
-                                     icon = icon('line-chart'))
-                       ),
+            #About Page                      
+                       menuItem(text = "About", tabName = "about", icon = icon("earth-americas")), 
+                       
+            
+            #charts          
+                       menuItem("Graphs & Tables", tabName = "charts", icon = icon("square-poll-vertical")),
+            
+ 
+            
+            #Data         
                        menuItem("Data", tabName = "data", icon = icon("database")), 
-                       menuItem("Click for Github Repository", href = "https://github.com/rpengling/SURV675_HW4.git", icon = icon("code"))
-                               )
-                    ), 
+                       
+
+               
+            #Github        
+                       menuItem("Click for Github Repository", href = "https://github.com/rpengling/SURV675_HW4.git", icon = icon("link"))
+                      )
+    ), 
     
     
-    dashboardBody(" ",
+    
+    
+    
+    
+    
+    dashboardBody(
                   tabItems(
-                    tabItem(tabName = "IB", 
-                            fluidRow(
-                              infoBoxOutput("min_", width = 3), #"min_" is defined in server file
-                              infoBoxOutput("max_", width = 3), #?validColors for list of color options
-                              infoBoxOutput("sd_", width = 3), 
-                              infoBoxOutput("mean_", width = 3)
-                            )),
+                    
+            #About        
+                    tabItem(tabName = "about", p("Quote1"), p("Quote2"),
+                            ),
+                    
+           
+            #Charts(Graphs)         
                     tabItem(tabName = "charts", 
                             fluidRow(
-                              tabBox(id = "tabchart1", 
-                                     tabPanel("Tab1", plotlyOutput("plot1")), 
-                                     tabPanel("Tab2", plotlyOutput("plot2")), 
-                                     tabPanel("Tab3", plotlyOutput("plot3")), 
-                                     tabPanel("Tab4", plotlyOutput("plot4"))
-                                     ), 
-                              tabBox(id = "tabchart2", side = "right", 
-                                     tabPanel("Tab1", "Tab 1 content"), 
-                                     tabPanel("Tab2", "Tab 2 content"), 
-                                     tabPanel("Tab3", "Tab 3 content")
-                                     )
-                            )),
-                    tabItem(tabName = "box", 
-                            #First row
-                            fluidRow(box(title = "box with a plot", plotlyOutput("plot1", height = 250))), 
-                            box(title = "Box with a plot", status = "primary", solidHeader = TRUE, plotlyOutput("plot2", height = 250)), 
-                            #Second row 
-                            fluidRow(box(title = "Box with datatable", tableOutput("data"), width = 8)), 
-                            box(title = "Box with input widget", uiOutput("inputwidget"), width = 4)), #Total row width = 12. 
-                    tabItem(tabName = "about", p(" "), p(" ")), 
-                    tabItem(tabName = "data", dataTableOutput("mydatatable"))
+                              box(title = "Box with input widget", status = "primary", solidHeader = T, uiOutput("inputwidget"), width = 4)),
+                              
+                              
+                            fluidRow(
+                              tabBox(id = "tabchart1", title = "Graphs",
+                                     tabPanel("Attitudes Towards Democracy", plotlyOutput("dem")), 
+                                     tabPanel("News Consumption", plotlyOutput("new")), 
+                                     tabPanel("Attitudes Towards Science", plotlyOutput("sci"))
+                                    ), 
+                              tabBox(id = "tabchart3", side = "right", title = "Tables",
+                                     tabPanel("Attitudes Towards Democracy", DT::dataTableOutput("demo")), 
+                                     tabPanel("News Consumption", DT::dataTableOutput("news")),
+                                     tabPanel("Attitudes Towards Science", DT::dataTableOutput("science"))
+                                    )
+                            ),
+                            
+                            fluidRow(
+                              tabBox(id = "tabchart2", title = "All Countries",
+                                     tabPanel("Attitudes Towards Democracy", DT::dataTableOutput("bigdem")), 
+                                     tabPanel("News Consumption", DT::dataTableOutput("bignew")), 
+                                     tabPanel("Attitudes Towards Science", DT::dataTableOutput("bigsci"))
+                                    )
+                              )
+                              
+                            
+                            
+                            
+                            
+                            
+                            
+                            ),
+                    
+
+            #Data
+                    tabItem(tabName = "data", DT::dataTableOutput("mydatatable"))
+                    
+                    
                     )
                  )
   )
